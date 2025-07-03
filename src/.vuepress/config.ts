c397f26edade8a01114babbe4ada49e2cfe68905
钱鹏展 <873373549@qq.com>
2025-07-03 21:37:14 +0800

    提交

--- src/.vuepress/config.ts
+++ src/.vuepress/config.ts
@@ -8,7 +8,12 @@ export default defineUserConfig({
   lang: "zh-CN",
   title: "学习笔记",
   description: "记录本人技术学习的笔记和总结",
-
+  head: [
+      // 设置 favor.ico，.vuepress/public 下
+      [
+        'link', { rel: 'icon', href: '/favicon.ico' }
+      ],
+    ],
   theme,
 
   // 和 PWA 一起启用
