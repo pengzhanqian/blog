cb3219354e353aedbaf7643350f3da3944001514
钱鹏展 <873373549@qq.com>
2025-07-04 14:30:39 +0800

    docs

--- docs/.vuepress/config.ts
+++ docs/.vuepress/config.ts
@@ -3,7 +3,7 @@ import { defineUserConfig } from "vuepress";
 import theme from "./theme.js";
 
 export default defineUserConfig({
-  base: "/blog/",
+  base: "/",
   dest: 'docs/.vuepress/dist',
   lang: "zh-CN",
   title: "学习笔记",
